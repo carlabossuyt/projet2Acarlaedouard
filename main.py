e1e01943a1ca1e4673048f84022700d686be4204
carla bossuyt <bossuytcarla03@gmail.com>
2023-06-02 15:52:55 +0200

    magnifique code

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
-
+import tableau
 
 if __name__ == "__main__":
+    sequencea = "ACTGTAG"
+    sequenceb = "ACGGCTAT"
+    grille1 = remplir(sequencea, sequenceb) #remplissage de la grille d'alignement
+    affiche(grille1, sequencea, sequenceb) #affichage de la grille d'alignement
--- tableau.py
+++ tableau.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import operator
 
 def remplir(sequence1, sequence2):
     """
@@ -34,7 +34,7 @@ def remplir(sequence1, sequence2):
     return grille
 
 
-def affiche(grille, sequence1, sequence2):
+def affiche(grille, sequence1, sequence2, cheminoptimal):
     """
     Affichage de la grille
     :param grille: np.array
@@ -45,14 +45,53 @@ def affiche(grille, sequence1, sequence2):
     fig, ax = plt.subplots()
     #suppression des axes
     ax.set_axis_off()
-    #creation d'une table avec la grill d'alignement , les séquences en tant que labels de lignes et de colonnes
-    ax.table(cellText=grille, rowLabels="-" + sequence1, colLabels="-" + sequence2, loc="center")
+    #creation d'une table avec la grille d'alignement , les séquences en tant que labels de lignes et de colonnes
+    table = ax.table(cellText=grille, rowLabels="-" + sequence1, colLabels="-" + sequence2, loc="center")
+    for x,y in cheminoptimal:
+        cellule = table.get_celld()[x+1, y]
+        cellule.set_facecolor("pink")
     #affichage de la figure
     plt.show()
+    print(grille)
+
+def remonter(grille, sequence1, sequence2):
+    def verif():
+        i, j= parcours[-1] #recupere les coordonnées de la dernière case
+        score=grille[x][y]
+        if position == 1 and ((max - 2) == score): #position haut
+            return True
+        elif position == 3 and ((max-2)==score): #position gauche
+            return True
+        elif position == 2 and sequence1[x-1] == sequence2[y-1] and max +2 == score: #match
+            return True
+        elif position == 2 and sequence1[x-1] != sequence2[y-1] and max -1 == score: #dismatch
+            return True
+        else:
+            return False
+    x= len(grille)-1
+    y = len(grille[0])-1 #on parcours les colonnes dans une ligne fixe
+    parcours=[(x,y)]
+    while x!=1 and y!=1:
+        antecedents=[((x,y-1), grille[x][y-1], 1), #antecedant haut = position 1
+                     ((x-1, y-1), grille[x-1][y-1], 2), #antecedant diago = position 2
+                     ((x-1, y), grille[x-1][y], 3)] ##antecedant gauche = position 3
+        antecedents.sort(key=operator.itemgetter(1), reverse=True) #permet de trier et sélectionner le + grand antecedents
+        for cord_ant_max, score_ant_max, position in antecedents:
+            max = score_ant_max
+            if verif():
+                parcours.append(cord_ant_max) #ajoute les coordonnées à la liste parcours
+                break #retire le maximum si il le score n'est pas bon
+        x, y =parcours[-1]
+    return parcours
+
 
 
 if __name__ == "__main__":
     sequencea = "ACTGTAG"
     sequenceb = "ACGGCTAT"
     grille1 = remplir(sequencea, sequenceb) #remplissage de la grille d'alignement
-    affiche(grille1, sequencea, sequenceb) #affichage de la grille d'alignement
+    cheminoptimal = remonter(grille1, sequencea, sequenceb)
+    affiche(grille1, sequencea, sequenceb, cheminoptimal) #affichage de la grille d'alignement
+
+
+    print(remonter(grille1, sequencea, sequenceb))
